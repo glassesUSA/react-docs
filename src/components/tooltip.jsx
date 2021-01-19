39e65015decb51456a7307a42ec6b129670ce380
Theo Freud <theo.f@glassesusa.com>
2021-01-19 19:17:59 +0200

    Fix font

--- src/components/tooltip.jsx
+++ src/components/tooltip.jsx
@@ -5,48 +5,63 @@ import Section from './common/section.jsx'
 class Tooltip extends Component {
   state = {}
 
-  componentDidMount() {
+  resizeTooltips() {
     document.querySelectorAll('.dsmTooltip').forEach((e) => {
-      let text = e.innerText
-      e.innerHTML = `<div class="item">?</div><div class="container">
+      setTimeout(() => {
+        if (e.querySelector('.item')) return
+
+        let text = e.innerText
+        e.innerHTML = `<div class="item">?</div><div class="container">
             <div class="arrow"></div>
             <div class="infoBox">
                 <div class="textArea">${text}</div>
             </div>
         </div>`
-      const el = e.querySelector('.container')
-      const containerHeight = el.clientHeight
-      const containerWidth = el.clientWidth
+        const el = e.querySelector('.container')
+        const containerHeight = el.clientHeight
+        const containerWidth = el.clientWidth
 
-      switch (true) {
-        case e.classList.contains('right'):
-          el.style.top = `-${containerHeight / 2 - 5}px`
-          el.style.left = `-${containerWidth}px`
-          el.querySelector('.arrow').style.top = `${containerHeight / 2 - 4}px`
-          break
-        case e.classList.contains('left'):
-          el.style.top = `-${containerHeight / 2 - 5}px`
-          el.style.right = `-${containerWidth}px`
-          el.querySelector('.arrow').style.top = `${containerHeight / 2 - 4}px`
-          el.style.left = `40px`
-          break
-        case e.classList.contains('bottom'):
-          el.style.top = `30px`
-          el.style.left = `-${containerWidth / 2 - 10}px`
-          break
-        default:
-          el.style.left = `-${containerWidth / 2 - 15}px`
-          el.style.top = `unset`
-          el.style.bottom = `30px`
-          break
-      }
+        switch (true) {
+          case e.classList.contains('right'):
+            el.style.top = `-${containerHeight / 2 - 5}px`
+            el.style.left = `-${containerWidth}px`
+            el.querySelector('.arrow').style.top = `${
+              containerHeight / 2 - 4
+            }px`
+            break
+          case e.classList.contains('left'):
+            el.style.top = `-${containerHeight / 2 - 5}px`
+            el.style.right = `-${containerWidth}px`
+            el.querySelector('.arrow').style.top = `${
+              containerHeight / 2 - 4
+            }px`
+            el.style.left = `40px`
+            break
+          case e.classList.contains('bottom'):
+            el.style.top = `30px`
+            el.style.left = `-${containerWidth / 2 - 10}px`
+            break
+          default:
+            el.style.left = `-${containerWidth / 2 - 15}px`
+            el.style.top = `unset`
+            el.style.bottom = `30px`
+            break
+        }
 
-      if (e.classList.contains('close')) {
-        el.innerHTML =
-          el.innerHTML +
-          `<svg onclick="closeTooltip(this)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13" width="100%" height="100%"><g fill="none" stroke="#b0bdc5"><path d="M12.3.7L.6 12.3M.7.7l11.7 11.6"></path></g></svg>`
-      }
+        if (e.classList.contains('close')) {
+          el.innerHTML =
+            el.innerHTML +
+            `<svg onclick="closeTooltip(this)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13" width="100%" height="100%"><g fill="none" stroke="#b0bdc5"><path d="M12.3.7L.6 12.3M.7.7l11.7 11.6"></path></g></svg>`
+        }
+      }, 100)
     })
+  }
+
+  componentDidMount() {
+    this.resizeTooltips()
+  }
+  componentDidUpdate() {
+    this.resizeTooltips()
   }
 
   render() {
