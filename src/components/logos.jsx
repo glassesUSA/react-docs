4bddf9cfb961a3a345987edd718376eaba0ddf44
Theo Freud <theo.f@glassesusa.com>
2021-02-10 17:26:20 +0200

    Add more

--- src/components/logos.jsx
+++ src/components/logos.jsx
@@ -9,7 +9,9 @@ class Logos extends Component {
   render() {
     return (
       <div>
-        <div className="pageTitle contentMaxWidth">Logos - UNDER CONSTRUCTION</div>
+        <div className="pageTitle contentMaxWidth">
+          Logos - UNDER CONSTRUCTION
+        </div>
         <div className="splitColumn contentMaxWidth">
           <div id="content">
             <div className="pageSubTitle">
@@ -22,71 +24,91 @@ class Logos extends Component {
               Logos can be coloured, resized and have any effects applied to it
               that a normal font would have.
               <Code>{'<i class="dsmIcons clock"></i>'}</Code>
-              <div className="exampleContainer iconGrid">
-                <CopyIcon prefix="dsmLogo" name="adidas" />
-                <CopyIcon prefix="dsmLogo" name="alain_mikli" />
-                <CopyIcon prefix="dsmLogo" name="armani_exchange" />
-                <CopyIcon prefix="dsmLogo" name="autoflex" />
-                <CopyIcon prefix="dsmLogo" name="balenciaga" />
-                <CopyIcon prefix="dsmLogo" name="blumarine" />
-                <CopyIcon prefix="dsmLogo" name="burberry" />
-                <CopyIcon prefix="dsmLogo" name="calvin_klein_ck" />
-                <CopyIcon prefix="dsmLogo" name="calvin_klein_cosmetics" />
-                <CopyIcon prefix="dsmLogo" name="calvin_klein" />
-                <CopyIcon prefix="dsmLogo" name="celine" />
-                <CopyIcon prefix="dsmLogo" name="coach" />
-                <CopyIcon prefix="dsmLogo" name="converse" />
-                <CopyIcon prefix="dsmLogo" name="diva" />
-                <CopyIcon prefix="dsmLogo" name="dkny" />
-                <CopyIcon prefix="dsmLogo" name="dsquared2" />
-                <CopyIcon prefix="dsmLogo" name="emporio_armani" />
-                <CopyIcon prefix="dsmLogo" name="ermenegildo_zenga" />
-                <CopyIcon prefix="dsmLogo" name="flexon" />
-                <CopyIcon prefix="dsmLogo" name="fossil" />
-                <CopyIcon prefix="dsmLogo" name="gant" />
-                <CopyIcon prefix="dsmLogo" name="giorgio_armani" />
-                <CopyIcon prefix="dsmLogo" name="givenchy" />
-                <CopyIcon prefix="dsmLogo" name="gucci" />
-                <CopyIcon prefix="dsmLogo" name="guess" />
-                <CopyIcon prefix="dsmLogo" name="hugo_boss" />
-                <CopyIcon prefix="dsmLogo" name="jimmy_choo" />
-                <CopyIcon prefix="dsmLogo" name="kate_spade" />
-                <CopyIcon prefix="dsmLogo" name="lacoste" />
-                <CopyIcon prefix="dsmLogo" name="lanvim" />
-                <CopyIcon prefix="dsmLogo" name="marc_jacobs" />
-                <CopyIcon prefix="dsmLogo" name="michael_kors" />
-                <CopyIcon prefix="dsmLogo" name="miu_miu" />
-                <CopyIcon prefix="dsmLogo" name="mont_blanc" />
-                <CopyIcon prefix="dsmLogo" name="nike" />
-                <CopyIcon prefix="dsmLogo" name="oakley" />
-                <CopyIcon prefix="dsmLogo" name="oneill" />
-                <CopyIcon prefix="dsmLogo" name="persol" />
-                <CopyIcon prefix="dsmLogo" name="pierre_cardin" />
-                <CopyIcon prefix="dsmLogo" name="polo_ralph_lauren" />
-                <CopyIcon prefix="dsmLogo" name="prada" />
-                <CopyIcon prefix="dsmLogo" name="puma" />
-                <CopyIcon prefix="dsmLogo" name="ray_ban" />
-                <CopyIcon prefix="dsmLogo" name="saint_laurent" />
-                <CopyIcon prefix="dsmLogo" name="stella_mccartney" />
-                <CopyIcon prefix="dsmLogo" name="super_dry" />
-                <CopyIcon prefix="dsmLogo" name="swarovski" />
-                <CopyIcon prefix="dsmLogo" name="tom_ford" />
-                <CopyIcon prefix="dsmLogo" name="tory_burch" />
-                <CopyIcon prefix="dsmLogo" name="versach" />
-                <CopyIcon prefix="dsmLogo" name="viktor_and_rolf" />
-                <CopyIcon prefix="dsmLogo" name="vogue" />
-                <CopyIcon prefix="dsmLogo" name="revel_tune" />
-                <CopyIcon prefix="dsmLogo" name="amelia_e" />
-                <CopyIcon prefix="dsmLogo" name="gamespex" />
-                <CopyIcon prefix="dsmLogo" name="gamespex2" />
-                <CopyIcon prefix="dsmLogo" name="muse_symbol" />
-                <CopyIcon prefix="dsmLogo" name="muse_x_hilary_duff" />
-                <CopyIcon prefix="dsmLogo" name="muse" />
-                <CopyIcon prefix="dsmLogo" name="olos" />
-                <CopyIcon prefix="dsmLogo" name="ottoto" />
-                <CopyIcon prefix="dsmLogo" name="revel" />
-                <CopyIcon prefix="dsmLogo" name="seaclean" />
-                <CopyIcon prefix="dsmLogo" name="yoji" />
+              <div className="exampleContainer iconGrid logoGrid">
+                <CopyIcon prefix="dsmLogo" mode="logo" name="adidas" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="alain_mikli" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="armani_exchange" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="autoflex" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="balenciaga" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="blumarine" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="burberry" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="calvin_klein_ck" />
+                <CopyIcon
+                  prefix="dsmLogo"
+                  mode="logo"
+                  name="calvin_klein_cosmetics"
+                />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="calvin_klein" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="celine" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="coach" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="converse" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="diva" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="dkny" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="dsquared2" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="emporio_armani" />
+                <CopyIcon
+                  prefix="dsmLogo"
+                  mode="logo"
+                  name="ermenegildo_zenga"
+                />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="flexon" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="fossil" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="gant" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="giorgio_armani" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="givenchy" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="gucci" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="guess" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="hugo_boss" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="jimmy_choo" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="kate_spade" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="lacoste" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="lanvim" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="marc_jacobs" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="michael_kors" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="miu_miu" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="mont_blanc" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="nike" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="oakley" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="oneill" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="persol" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="pierre_cardin" />
+                <CopyIcon
+                  prefix="dsmLogo"
+                  mode="logo"
+                  name="polo_ralph_lauren"
+                />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="prada" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="puma" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="ray_ban" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="saint_laurent" />
+                <CopyIcon
+                  prefix="dsmLogo"
+                  mode="logo"
+                  name="stella_mccartney"
+                />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="super_dry" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="swarovski" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="tom_ford" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="tory_burch" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="versach" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="viktor_and_rolf" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="vogue" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="revel_tune" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="amelia_e" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="gamespex" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="gamespex2" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="muse_symbol" />
+                <CopyIcon
+                  prefix="dsmLogo"
+                  mode="logo"
+                  name="muse_x_hilary_duff"
+                />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="muse" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="olos" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="ottoto" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="revel" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="seaclean" />
+                <CopyIcon prefix="dsmLogo" mode="logo" name="yoji" />
               </div>
             </Section>
           </div>
